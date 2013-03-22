0d6f078fe9ddf1bba81aa98692f5e43614276fe0
Jose Rivera <jriverac@me.com>
2013-03-21 20:29:04 -0600

    HTTP Basic Authentication Enabled.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
 	
 
   	protect_from_forgery
+    USERS = { "jriverac" => "safe4mat" }
+
+      before_filter :authenticate
+      private
+
+      def authenticate
+        authenticate_or_request_with_http_digest do |username|
+          USERS[username]
+        end
+      end
 
 
 end
